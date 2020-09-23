9557d6e9a175fa710c1f9ece573a3eeeba50f3e1
KCorrea091 <KCorrea091@outlook.com>
2020-09-22 21:13:07 -0400

    primer commit

---
+++ ThreadEjemplo2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Threading;
+
+namespace ThreadEjemplo2
+{
+    class Program
+    {
+        const string path = @"";
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<250; i++)
+            {
+                ThreadPool.QueueUserWorkItem(CrearArchivos, i);
+
+            }
+            while (ThreadPool.PendingWorkItemCount > 0) ;
+        }
+        static void CrearArchivos(object data)
+        {
+            int i = (int)data;
+
+            using(var sw = new StreamWriter(path +i+ "txt"))
+            {
+                sw.WriteLine($"Este es el hilo {Thread.CurrentThread.ManagedThreadId}");
+            }
+            Console.WriteLine($"Hilo # {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
---
+++ TreadPoolSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Threading;
+
+namespace TreadPoolSample
+{
+    class Program
+    {
+        const string path =@"";
+        static void Main(string[] args)
+        {
+            
+            //int max, c = 0;
+            //ThreadPool.GetMaxThreads(out max, out c);
+            //Console.WriteLine(max);
+
+            for(int i = 0; i<50; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Create, i);
+            }
+            // Este while mantiene trabajando a el programa mientras haya tareas pendientes w
+            while (ThreadPool.PendingWorkItemCount > 0);
+        }
+
+        static void Create(object data)
+        {
+            int i = (int)data;
+            using (var sw = new StreamWriter(path +i+"txt"))
+            {
+               sw.WriteLine($"Este es el hilo {Thread.CurrentThread.ManagedThreadId}");
+            }
+            Console.WriteLine($"Hilo {Thread.CurrentThread.ManagedThreadId} ");
+
+        }
+    }
+}
